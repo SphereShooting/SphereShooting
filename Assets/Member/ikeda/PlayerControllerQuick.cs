6a2b3ec708c2e7700105daaff82a54b9e422ca34
LightToRingo <lightblooooo@gmail.com>
2020-03-27 19:02:50 +0900

    PlayerController3種類実装
    Quick => レスポンス性重視のコントローラー 自動無
    X => 縦軸自動回転 横軸移動のみ　レスポンス×
    PlayerController => 慣性あり 自動無

---
+++ Assets/Member/ikeda/PlayerControllerQuick.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GodTouches {
+    public class PlayerControllerQuick : MonoBehaviour
+    {
+        public GameObject stage;
+        Vector2 startPos;
+        Quaternion startRot;
+        float width, height;
+        float vertical, horizontal;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            width = Screen.width;
+            height = Screen.height;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var phase = GodTouch.GetPhase();
+            if(phase == GodPhase.Began)
+            {
+                startPos = GodTouch.GetPosition();
+                startRot = stage.transform.rotation;
+            }
+            else if(phase == GodPhase.Moved)
+            {
+                horizontal = (GodTouch.GetPosition().x - startPos.x) / width;
+                vertical = (GodTouch.GetPosition().y - startPos.y) / height;
+                stage.transform.rotation = startRot;
+                stage.transform.Rotate(new Vector3(90 * vertical, -90 * horizontal, 0), Space.World);
+            }
+        }
+    }
+}
---
+++ Assets/Member/ikeda/PlayerControllerX.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+namespace GodTouches
+{
+    public class PlayerControllerX : MonoBehaviour
+    {
+        private Rigidbody rb;
+        private float sPos, ePos;
+        float vX, vY, vZ;
+        float xMin, xMax;
+        float yMin, yMax;
+        bool trigger;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            rb = gameObject.GetComponent<Rigidbody>();
+            xMin = 0.2f; xMax = 1.0f;
+            yMin = 0.2f; yMax = 1.0f;
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            var phase = GodTouch.GetPhase();
+            vY = -1.0f;
+
+            //タッチされたときとあとの処理
+            if (phase == GodPhase.Began)
+            {
+                //タッチ開始
+                sPos = GodTouch.GetPosition().x;
+            }
+            else if (phase == GodPhase.Ended)
+            {
+                //タッチされた場所の開始位置と最終位置を比較して上ならspeedを上げる
+                //下ならspeedを下げる
+                ePos = GodTouch.GetPosition().x;
+                trigger = true;
+                
+            }
+
+            //タッチされている間の処理
+            if (phase == GodPhase.Stationary)
+            {
+                Debug.Log("Idle");
+                vY = -0.2f;
+                
+            }else if (Input.GetMouseButton(0))
+            {
+                Debug.Log("Idle");
+                vY = -0.2f;
+                
+            }
+
+            if(sPos > ePos && trigger)
+            {
+                vX += 0.3f;
+                trigger = false;
+                Debug.Log(vX);
+            }
+            else if(sPos < ePos && trigger)
+            {
+                vX -= 0.3f;
+                trigger = false;
+                Debug.Log(vX);
+            }
+            rb.angularVelocity = new Vector3(vY, vX, vZ);
+        }
+    }
+}
